d9cf5b0f68746a80878f4486c037c9c230e0a9a7
Bogstag <krister@bogstag.se>
2016-02-11 23:50:17 +0100

    removed illuminate/html

--- config/app.php
+++ config/app.php
@@ -196,8 +196,6 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Form'      => 'Illuminate\Html\FormFacade',
-        'HTML'      => 'Illuminate\Html\HtmlFacade',
         'Datatables'      => 'yajra\Datatables\Datatables',
     ],
 
