6f891759e7618b1dbb5add29494da23b3ed6efc8
Bogstag <krister@bogstag.se>
2016-02-13 00:51:13 +0100

    Added some more checks when upsert

--- app/Http/Controllers/Integration/SteamAPI/SteamOwnedGames.php
+++ app/Http/Controllers/Integration/SteamAPI/SteamOwnedGames.php
@@ -25,23 +25,36 @@ class SteamOwnedGames extends SteamAPI
                 $SteamGame = new Steamgame;
                 $SteamGame->id = $game['appid'];
             }
-            $SteamGame->name = $game['name'];
-            $SteamGame->playtimeforever = $game['playtime_forever'];
+
+            if (isset($game['name'])) {
+                $SteamGame->name = $game['name'];
+            }
+
+            if (isset($game['playtime_forever'])) {
+                $SteamGame->playtimeforever = $game['playtime_forever'];
+            }
+
             if (isset($game['playtime_2weeks'])) {
                 $SteamGame->playtime2weeks = $game['playtime_2weeks'];
             } else {
                 $SteamGame->playtime2weeks = 0;
             }
+
             $imgUrl = 'http://media.steampowered.com/steamcommunity/public/images/apps/' . $SteamGame->id . '/';
-            $SteamGame->iconurl = $imgUrl . $game['img_icon_url'] . '.jpg';
-            $SteamGame->logourl = $imgUrl . $game['img_logo_url'] . '.jpg';
+            if (isset($game['img_icon_url'])) {
+                $SteamGame->iconurl = $imgUrl . $game['img_icon_url'] . '.jpg';
+            }
+            
+            if (isset($game['img_logo_url'])) {
+                $SteamGame->logourl = $imgUrl . $game['img_logo_url'] . '.jpg';
+            }
+
             if (isset($game['has_community_visible_stats'])) {
                 $SteamGame->hasstats = $game['has_community_visible_stats'];
             } else {
-                $SteamGame->hasstats = False;
+                $SteamGame->hasstats = false;
             }
             $SteamGame->save();
-
         }
     }
 }
