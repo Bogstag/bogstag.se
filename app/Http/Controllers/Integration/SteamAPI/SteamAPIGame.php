6c12e302f271cd8a630cc9762f5c8d8b39d7cc10
Bogstag <krister@bogstag.se>
2017-03-05 01:12:51 +0100

    fixed so no error is thrown when im not playing for long time

--- app/Http/Controllers/Integration/SteamAPI/SteamAPIGame.php
+++ app/Http/Controllers/Integration/SteamAPI/SteamAPIGame.php
@@ -35,65 +35,32 @@ class SteamAPIGame extends SteamAPI
     }
 
     /**
-     * Can be used for testing and loading a single game by id.
-     *
-     * @param null $gameId
-     *
-     * @return string
+     * @return mixed
      */
-    public function loadGame($gameId = null)
+    public function getOwnedGames()
     {
-        if ($gameId === null) {
-            return 'No game id specified';
-        }
-        $SteamOwnedGamesFromAPI = $this->getOwnedGames();
-        foreach ($SteamOwnedGamesFromAPI as $game) {
-            if ($gameId == $game->appid) {
-                break;
-            }
-        }
-        $SteamGame = SteamGame::firstOrNew(['id' => $game->appid]);
-        $this->parseAndSaveSteamGame($SteamGame, $game);
-        $this->runDescriptionSchemaAchievements($SteamGame);
-    }
+        $localfile = 'SteamApi/GetOwnedGames.json';
 
-    /**
-     * Not really needed to run this.
-     * You can use it as a slow update of your db if you like
-     * updated screenshots and movies and achievements.
-     */
-    public function updateGames()
-    {
-        $this->parseAndSaveOnlyOwnedGames();
-        new SteamAPIGameDescription(SteamGame::DescriptionNeedUpdate()->first());
-        new SteamAPIGameSchema(SteamGame::SchemaNeedUpdate()->first());
-        new SteamAPIGameAchievements(SteamGame::AchievementsNeedUpdate()->first());
-    }
-
-    /**
-     * Can be scheduled on a tighter interval.
-     * Update only games that you have played last 2 weeks.
-     * This also runs and updates all games (not decription, schema or achievements)
-     * Needed because RecentlyPlayedGames is missing has_community_visible_stats.
-     */
-    public function updateRecentlyPlayedGames()
-    {
-        $this->parseAndSaveOnlyOwnedGames();
-        $SteamOwnedGamesFromAPI = $this->getRecentlyPlayedGames();
-
-        foreach ($SteamOwnedGamesFromAPI as $game) {
-            if (empty($game)) {
-                continue;
-            }
-            $SteamGame = SteamGame::firstOrNew(['id' => $game->appid]);
-            $this->parseAndSaveSteamGame($SteamGame, $game);
-            $this->runDescriptionSchemaAchievements($SteamGame);
+        if (env('APP_ENV', false) == 'local' && $cachedAPICall = $this->getCachedAPICall($localfile)) {
+            return $cachedAPICall;
         }
+
+        $GetOwnedGames = new GetOwnedGames(env('STEAM_64ID', false));
+        $GetOwnedGames = $this->steam->run($GetOwnedGames->setIncludeAppInfo(1));
+        $GetOwnedGamesJson = json_encode($GetOwnedGames['response']['games']);
+
+        $this->incrementSteamApiLimitCounter();
+
+        if ((env('APP_ENV', false) == 'local')) {
+            $this->saveCachedAPICall($localfile, $GetOwnedGamesJson);
+        }
+
+        return json_decode($GetOwnedGamesJson);
     }
 
     /**
      * @param SteamGame $SteamGame
-     * @param $game
+     * @param           $game
      *
      * @return bool
      */
@@ -137,6 +104,52 @@ class SteamAPIGame extends SteamAPI
         return false;
     }
 
+    /**
+     * @param $SteamGame
+     */
+    public function runDescriptionSchemaAchievements($SteamGame)
+    {
+        new SteamAPIGameDescription($SteamGame);
+        new SteamAPIGameSchema($SteamGame);
+        new SteamAPIGameAchievements($SteamGame);
+    }
+
+    /**
+     * Can be used for testing and loading a single game by id.
+     *
+     * @param null $gameId
+     *
+     * @return string
+     */
+    public function loadGame($gameId = null)
+    {
+        if ($gameId === null) {
+            return 'No game id specified';
+        }
+        $SteamOwnedGamesFromAPI = $this->getOwnedGames();
+        foreach ($SteamOwnedGamesFromAPI as $game) {
+            if ($gameId == $game->appid) {
+                break;
+            }
+        }
+        $SteamGame = SteamGame::firstOrNew(['id' => $game->appid]);
+        $this->parseAndSaveSteamGame($SteamGame, $game);
+        $this->runDescriptionSchemaAchievements($SteamGame);
+    }
+
+    /**
+     * Not really needed to run this.
+     * You can use it as a slow update of your db if you like
+     * updated screenshots and movies and achievements.
+     */
+    public function updateGames()
+    {
+        $this->parseAndSaveOnlyOwnedGames();
+        new SteamAPIGameDescription(SteamGame::DescriptionNeedUpdate()->first());
+        new SteamAPIGameSchema(SteamGame::SchemaNeedUpdate()->first());
+        new SteamAPIGameAchievements(SteamGame::AchievementsNeedUpdate()->first());
+    }
+
     private function parseAndSaveOnlyOwnedGames()
     {
         $SteamOwnedGamesFromAPI = $this->getOwnedGames();
@@ -150,41 +163,29 @@ class SteamAPIGame extends SteamAPI
     }
 
     /**
-     * @param $SteamGame
+     * Can be scheduled on a tighter interval.
+     * Update only games that you have played last 2 weeks.
+     * This also runs and updates all games (not decription, schema or achievements)
+     * Needed because RecentlyPlayedGames is missing has_community_visible_stats.
      */
-    public function runDescriptionSchemaAchievements($SteamGame)
+    public function updateRecentlyPlayedGames()
     {
-        new SteamAPIGameDescription($SteamGame);
-        new SteamAPIGameSchema($SteamGame);
-        new SteamAPIGameAchievements($SteamGame);
+        $this->parseAndSaveOnlyOwnedGames();
+        $SteamOwnedGamesFromAPI = $this->getRecentlyPlayedGames();
+            return;
+        }
+        foreach ($SteamOwnedGamesFromAPI as $game) {
+            if (empty($game)) {
+                continue;
+            }
+            $SteamGame = SteamGame::firstOrNew(['id' => $game->appid]);
+            $this->parseAndSaveSteamGame($SteamGame, $game);
+            $this->runDescriptionSchemaAchievements($SteamGame);
+        }
     }
 
     /**
-     * @return mixed
-     */
-    public function getOwnedGames()
-    {
-        $localfile = 'SteamApi/GetOwnedGames.json';
-
-        if (env('APP_ENV', false) == 'local' && $cachedAPICall = $this->getCachedAPICall($localfile)) {
-            return $cachedAPICall;
-        }
-
-        $GetOwnedGames = new GetOwnedGames(env('STEAM_64ID', false));
-        $GetOwnedGames = $this->steam->run($GetOwnedGames->setIncludeAppInfo(1));
-        $GetOwnedGamesJson = json_encode($GetOwnedGames['response']['games']);
-
-        $this->incrementSteamApiLimitCounter();
-
-        if ((env('APP_ENV', false) == 'local')) {
-            $this->saveCachedAPICall($localfile, $GetOwnedGamesJson);
-        }
-
-        return json_decode($GetOwnedGamesJson);
-    }
-
-    /**
-     * @return mixed|void
+     * @return mixed|null
      */
     public function getRecentlyPlayedGames()
     {
@@ -196,11 +197,10 @@ class SteamAPIGame extends SteamAPI
         $GetRecentlyPlayedGames = $this->steam->run($GetRecentlyPlayedGames);
 
         if (empty($GetRecentlyPlayedGames['response']['games'])) {
-            return;
+            return null;
         }
 
         $GetRecentlyPlayedGamesJson = json_encode($GetRecentlyPlayedGames['response']['games']);
-
         $this->incrementSteamApiLimitCounter();
 
         if ((env('APP_ENV', false) == 'local')) {
