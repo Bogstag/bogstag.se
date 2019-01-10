0af9385ac2ca1b3e32f189540d3bbd371784bacb
Bogstag <krister@bogstag.se>
2019-01-10 11:10:35 +0100

    remove email from nav

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -35,13 +35,6 @@
                 </li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                       aria-expanded="false"><i class="fa fa-server"></i> Server<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="{{ URL::secure('server/email') }}"><i class="fa fa-envelope-o"></i> Email</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                        aria-expanded="false"><i class="fa fa-film"></i> Movie<span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><a href="{{ URL::secure('movie/watched/all') }}"><i class="fa fa-eye"></i> Watched</a></li>
